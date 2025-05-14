85ea4f5469df1567505aa866e1279902d08fee80
mbangunProgram <programkedepan@gmail.com>
2025-05-14 11:55:38 +0700

    menambah 3 file

---
+++ Flora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulasan;
+
+/**
+ *
+ * @author DEDDY PC
+ */
+public class Flora {
+//    property / variabel
+//    method
+//    modifier 
+
+    String nama="Kelengkeng",buah,batang;
+    int usia;
+    
+    
+    
+}
